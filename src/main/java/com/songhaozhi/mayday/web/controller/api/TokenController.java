d400da93d96b94c1c0775c5c44f1c0d8f8e56d50
LittleHippo2 <haol@boyuecorp.com>
2020-03-10 11:18:33 +0800

    修改同步组织机构bug

--- src/main/java/com/songhaozhi/mayday/web/controller/api/TokenController.java
+++ src/main/java/com/songhaozhi/mayday/web/controller/api/TokenController.java
@@ -104,7 +104,7 @@ public class TokenController extends BaseController {
         try{
             url = url+"?access_token="+access_token;
             url = url+"&starttime="+starttime;
-            if(department.equals("") || department.equals("null")){
+            if(department == null || "".equals(department)){
                 url = url+"&department=";
             }else{
                 url = url+"&department="+department;
@@ -112,91 +112,107 @@ public class TokenController extends BaseController {
 
             String request = HttpUtil.get(BaseUrl+url,inParam,3000,3000);
             result = JSONObject.parseObject(request);
+
             //同步组织机构信息
             JSONArray org = JSONObject.parseObject(request).getJSONArray("org");
-            List<Org> orgList = new ArrayList<>();
-            for(int i= 0; i<org.size(); i++){
-                Org org1 = new Org();
-                org1.setOrganId(((JSONObject)org.get(i)).getString("organId"));
-                org1.setPath(((JSONObject)org.get(i)).getString("p"));
-                org1.setOrganName(((JSONObject)org.get(i)).getString("organName"));
-                org1.setOrgUuid(((JSONObject)org.get(i)).getString("orguuid"));
-                org1.setCode(((JSONObject)org.get(i)).getString("code"));
-                org1.setOrderId(((JSONObject)org.get(i)).getString("orderId"));
-                org1.setIsDelete(((JSONObject)org.get(i)).getString("isDelete"));
-                org1.setFatherId(((JSONObject)org.get(i)).getString("fatherId"));
-                org1.setDn(((JSONObject)org.get(i)).getString("dn"));
-                org1.setType(((JSONObject)org.get(i)).getString("type"));
-                org1.setTimesTamp(((JSONObject)org.get(i)).getString("timestamp"));
-                orgList.add(org1);
-            }
-            //同步人员信息
             JSONArray user = JSONObject.parseObject(request).getJSONArray("user");
-
-            List<User> userList = new ArrayList<>();
-            List<Relation> relationList = new ArrayList<>();
-            List<JsonUser> jsonUserList = new ArrayList<>();
-            for(int i = 0; i<user.size(); i++){
-                JsonUser jsonUser = new JsonUser();
-                jsonUser.setUserid(((JSONObject) user.get(i)).getString("userid"));
-                jsonUser.setFullname(((JSONObject) user.get(i)).getString("fullname"));
-                jsonUser.setAccount(((JSONObject) user.get(i)).getString("account"));
-                jsonUser.setPassword(((JSONObject) user.get(i)).getString("password"));
-                jsonUser.setSex(((JSONObject) user.get(i)).getString("sex"));
-                jsonUser.setOrganId(((JSONObject) user.get(i)).getString("organId"));
-                jsonUser.setOrderId(((JSONObject) user.get(i)).getString("orderId"));
-                jsonUser.setDn(((JSONObject) user.get(i)).getString("dn"));
-                jsonUser.setIsDelete(((JSONObject) user.get(i)).getString("isDelete"));
-                jsonUser.setCa(((JSONObject) user.get(i)).getString("ca"));
-                jsonUser.setIsManager(((JSONObject) user.get(i)).getString("isManager"));
-                jsonUser.setTokenId(((JSONObject) user.get(i)).getString("tokenId"));
-                jsonUser.setSpId(((JSONObject) user.get(i)).getString("spId"));
-                jsonUser.setSn(((JSONObject) user.get(i)).getString("sn"));
-                jsonUser.setIp(((JSONObject) user.get(i)).getString("ip"));
-                jsonUser.setStartDate(new Date());
-                jsonUser.setUserUuid(((JSONObject) user.get(i)).getString("userUuid"));
-                jsonUser.setUserEmail(((JSONObject) user.get(i)).getString("userEmail"));
-                jsonUser.setSecLevel(((JSONObject) user.get(i)).getString("secLevel"));
-                jsonUser.setFailedLoginCount(((JSONObject) user.get(i)).getString("failedLoginCount"));
-                jsonUser.setEditPwdTime(((JSONObject) user.get(i)).getString("editPwdTime"));
-                jsonUser.setMobile(((JSONObject) user.get(i)).getString("mobile"));
-                jsonUser.setTimestamp(((JSONObject) user.get(i)).getString("timestamp"));
-                jsonUser.setType(((JSONObject) user.get(i)).getString("type"));
-                jsonUser.setEndDate(new Date());
-                jsonUserList.add(jsonUser);
-
-
-                User user1 = new User();
-                user1.setUserid(((JSONObject) user.get(i)).getString("userid"));
-                user1.setFullname(((JSONObject) user.get(i)).getString("fullname"));
-                user1.setUserEmail(((JSONObject) user.get(i)).getString("userEmail"));
-                user1.setAccount(((JSONObject) user.get(i)).getString("account"));
-                user1.setPassword(((JSONObject) user.get(i)).getString("password"));
-
-
-                JSONArray relationArray = ((JSONObject) user.get(i)).getJSONArray("relation");
-                Relation relation = JSON.toJavaObject((JSON) relationArray.get(0),Relation.class);
-
-
-                relationList.add(relation);
-                userList.add(user1);
+            List<Org> orgList = null;
+            List<User> userList = null;
+            List<Relation> relationList = null;
+            List<JsonUser> jsonUserList = null;
+            if(org.size() ==0){
+                result.put("org", "没有可以同步的组织机构信息");
+            }else{
+                orgList = new ArrayList<>();
+                for(int i= 0; i<org.size(); i++){
+                    Org org1 = new Org();
+                    org1.setOrganId(((JSONObject)org.get(i)).getString("organId"));
+                    org1.setPath(((JSONObject)org.get(i)).getString("p"));
+                    org1.setOrganName(((JSONObject)org.get(i)).getString("organName"));
+                    org1.setOrgUuid(((JSONObject)org.get(i)).getString("orguuid"));
+                    org1.setCode(((JSONObject)org.get(i)).getString("code"));
+                    org1.setOrderId(((JSONObject)org.get(i)).getString("orderId"));
+                    org1.setIsDelete(((JSONObject)org.get(i)).getString("isDelete"));
+                    org1.setFatherId(((JSONObject)org.get(i)).getString("fatherId"));
+                    org1.setDn(((JSONObject)org.get(i)).getString("dn"));
+                    org1.setType(((JSONObject)org.get(i)).getString("type"));
+                    org1.setTimesTamp(((JSONObject)org.get(i)).getString("timestamp"));
+                    orgList.add(org1);
+                }
+                if(orgList.size() != 0){
+                    //同步gxzcc——org表
+                    orgService.insertOrgData(orgList);
+                }
             }
-            if(orgList.size() != 0){
-                //同步gxzcc——org表
-                orgService.insertOrgData(orgList);
+
+            if(user.size() == 0){
+                result.put("user","没有可以同步的人员信息");
+            }else{
+                //同步人员信息
+                userList = new ArrayList<>();
+                relationList = new ArrayList<>();
+                jsonUserList = new ArrayList<>();
+                for(int i = 0; i<user.size(); i++){
+                    JsonUser jsonUser = new JsonUser();
+                    jsonUser.setUserid(((JSONObject) user.get(i)).getString("userid"));
+                    jsonUser.setFullname(((JSONObject) user.get(i)).getString("fullname"));
+                    jsonUser.setAccount(((JSONObject) user.get(i)).getString("account"));
+                    jsonUser.setPassword(((JSONObject) user.get(i)).getString("password"));
+                    jsonUser.setSex(((JSONObject) user.get(i)).getString("sex"));
+                    jsonUser.setOrganId(((JSONObject) user.get(i)).getString("organId"));
+                    jsonUser.setOrderId(((JSONObject) user.get(i)).getString("orderId"));
+                    jsonUser.setDn(((JSONObject) user.get(i)).getString("dn"));
+                    jsonUser.setIsDelete(((JSONObject) user.get(i)).getString("isDelete"));
+                    jsonUser.setCa(((JSONObject) user.get(i)).getString("ca"));
+                    jsonUser.setIsManager(((JSONObject) user.get(i)).getString("isManager"));
+                    jsonUser.setTokenId(((JSONObject) user.get(i)).getString("tokenId"));
+                    jsonUser.setSpId(((JSONObject) user.get(i)).getString("spId"));
+                    jsonUser.setSn(((JSONObject) user.get(i)).getString("sn"));
+                    jsonUser.setIp(((JSONObject) user.get(i)).getString("ip"));
+                    jsonUser.setStartDate(new Date());
+                    jsonUser.setUserUuid(((JSONObject) user.get(i)).getString("userUuid"));
+                    jsonUser.setUserEmail(((JSONObject) user.get(i)).getString("userEmail"));
+                    jsonUser.setSecLevel(((JSONObject) user.get(i)).getString("secLevel"));
+                    jsonUser.setFailedLoginCount(((JSONObject) user.get(i)).getString("failedLoginCount"));
+                    jsonUser.setEditPwdTime(((JSONObject) user.get(i)).getString("editPwdTime"));
+                    jsonUser.setMobile(((JSONObject) user.get(i)).getString("mobile"));
+                    jsonUser.setTimestamp(((JSONObject) user.get(i)).getString("timestamp"));
+                    jsonUser.setType(((JSONObject) user.get(i)).getString("type"));
+                    jsonUser.setEndDate(new Date());
+                    jsonUserList.add(jsonUser);
+
+
+                    User user1 = new User();
+                    user1.setUserid(((JSONObject) user.get(i)).getString("userid"));
+                    user1.setFullname(((JSONObject) user.get(i)).getString("fullname"));
+                    user1.setUserEmail(((JSONObject) user.get(i)).getString("userEmail"));
+                    user1.setAccount(((JSONObject) user.get(i)).getString("account"));
+                    user1.setPassword(((JSONObject) user.get(i)).getString("password"));
+
+
+                    JSONArray relationArray = ((JSONObject) user.get(i)).getJSONArray("relation");
+                    Relation relation = JSON.toJavaObject((JSON) relationArray.get(0),Relation.class);
+
+
+                    relationList.add(relation);
+                    userList.add(user1);
+                }
+
+                if(userList.size() != 0){
+                    //同步mayday_user 表
+                    personService.insertUser(userList);
+                }
+                if(relationList.size() != 0){
+                    //同步GXZCC_SYS_ORGAN_USER_RELATION 表信息
+                    personService.insertRelationData(relationList);
+                }
+                if(jsonUserList.size() != 0){
+                    //同步gxzcc_user 表
+                    personService.insertGxzccUserData(jsonUserList);
+                }
+
             }
-            if(userList.size() != 0){
-                //同步mayday_user 表
-                personService.insertUser(userList);
-            }
-            if(relationList.size() != 0){
-                //同步GXZCC_SYS_ORGAN_USER_RELATION 表信息
-                personService.insertRelationData(relationList);
-            }
-            if(jsonUserList.size() != 0){
-                //同步gxzcc_user 表
-                personService.insertGxzccUserData(jsonUserList);
-            }
+
 
         }catch (DuplicateKeyException e ){
             result.put("error","数据插入数据库时，存在和数据库相同的数据，请输入准确的时间和部门id，确保数据不重复");
